1117158c32db0cc7e445fe5532cb9b653fd4acea
xmly <xmly@xmlydeMacBook-Pro.local>
2020-11-18 19:25:51 +0800

    add dynamic import

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ import store from './store';
 // import { Provider } from './utils/myReactRedux';
 // import store from './store/myStore';
 import ReactDOM from 'react-dom';
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import AddNum from './page/addNum';
-import ReduxApp from './page/redux';
+// import ReduxApp from './page/redux';
 import PrivateRoute from './router/privateRoute'
 import ContextApp from './page/context';
 import Login from './page/loginPage';
-import Cart from './page/cart';
+// import Cart from './page/cart';
 import Fp from './page/fp';
 // import Hooks from './page/hooks';
 import { 
@@ -24,20 +24,18 @@ import {
   Link 
 } from 'react-router-dom';
 
-import(/*webpackChunkName: "ramda" */'ramda').then(_ => {
-  console.log('-------------- _ --------->', _);
-  const add = _.curry((x, y) => x + y);
-  console.log('---------- add(1)(2) ---------> ', add(1)(2));
-})
 
-const getHooks = (location,cb) => {
-  console.log(1111111111111)
-  import(/*webpackChunkName: "hooks" */'./page/hooks').then(res => {
-    console.log('res: ', res)
-    cb(null, res);
-  });
+const Hooks = lazy(() => import(/*webpackChunkName: "hooks" */ './page/hooks'));
+const ReduxApp = lazy(() => import(/*webpackChunkName: "reduxApp" */ './page/redux'));
+const Cart = lazy(() => import(/*webpackChunkName: "cart" */ './page/cart'));
+
+const getComponent = Com => {
+  return (
+    <Suspense fallback={<div>loading...</div>}>
+      <Com />
+    </Suspense>
+  )
 }
-
 const App = props => {
   return (
     <Provider store={store}>
@@ -62,11 +60,11 @@ const App = props => {
           </Link>
         <Switch>
           <Route exact path="/" component={ContextApp} />
-          <Route exact path="/redux" component={ReduxApp} />
+          <Route exact path="/redux" component={() => getComponent(ReduxApp)} />
           <Route exact path="/login" component={Login} />
-          <Route exact path="/cart" component={Cart} />
+          <Route exact path="/cart" component={() => getComponent(Cart)} />
           <Route exact path="/fp" component={Fp} />
-          <Route exact path="/hooks" getComponent={getHooks} />
+          <Route exact path="/hooks" component={() => getComponent(Hooks)} />
           <PrivateRoute path="/search" component={AddNum}/>
           {/* <Route exact path="/search/:id" component={AddNum} /> */}
         </Switch>
---
+++ src/router/config.js
@@ -0,0 +1,12 @@
+
+
+const config = [
+    {
+        path: '/',
+        component: () => {},
+        exact: true
+    }
+]
+
+
+export default config;
