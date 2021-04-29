c90bc872cd7cb38b8c81f023e92db4336ad542c0
Chris.Cheng <chris.cheng@ximalaya.com>
2021-04-29 18:28:06 +0800

    test: 测试prettier

--- src/page/example/index.tsx
+++ src/page/example/index.tsx
@@ -1,27 +1,27 @@
 import React, { useState, useCallback, FC } from "react";
 
 const Example: FC = () => {
-  const [num, setNum] = useState(0);
-  const callbackNum = useCallback(() => {}, [num]);
-  return (
-    <div>
-      {num}
-      <button
-        onClick={() => {
-          setNum(num + 1);
-        }}
-      >
-        add
-      </button>
-      <button
-        onClick={() => {
-          console.log(callbackNum);
-        }}
-      >
-        add
-      </button>
-    </div>
-  );
+    const [num, setNum] = useState(0);
+    const callbackNum = useCallback(() => {}, [num]);
+    return (
+        <div>
+            {num}
+            <button
+                onClick={() => {
+                    setNum(num + 1);
+                }}
+            >
+                add
+            </button>
+            <button
+                onClick={() => {
+                    console.log(callbackNum);
+                }}
+            >
+                add
+            </button>
+        </div>
+    );
 };
-
+console.log(1);
 export default Example;
